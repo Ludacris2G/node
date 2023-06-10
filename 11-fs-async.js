b52ba3a450228a7787c5cf03c5b4c22b2fe10af6
Liviu <liviutoma946@gmail.com>
2023-06-11 01:39:27 +0300

    added async file

---
+++ 11-fs-async.js
@@ -0,0 +1,27 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, res) => {
+    if (err) {
+        console.log(err);
+        return;
+    } 
+    const first = res;
+    readFile('./content/second.txt', 'utf8', (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const second = res;
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first}, ${second}`,
+            { flag: 'a' }
+            ,(err, res) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('Here is the result: ', res);
+            })
+    })
+})
