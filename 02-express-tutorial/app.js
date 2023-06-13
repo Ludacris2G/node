5db7014d889fd5a733c78f795dff89fb181be549
Liviu <liviutoma946@gmail.com>
2023-06-14 02:26:36 +0300

    response api

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,56 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const { products } = require('./data');
+
+app.get('/', (req, res) => {
+    res.send('<h1> yo </h1><a href="/api/products">products</a>');
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image }
+    });
+    res.json(newProducts);
+})
+
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req.params);
+    const {productID} = req.params;
+
+    const singleProduct = products.find((product) => product.id === Number(productID));
+    if (!singleProduct) {
+        return res.status(404).send('Product Does Not Exist');
+    }
+    
+    return res.json(singleProduct);
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params);
+    res.send('Hello World');
+})
+
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query);
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if (sortedProducts.length < 1) {
+        // res.status(200).send('no products matched');
+        return res.status(200).json({ success:true, data:[] })
+    }
+    res.status(200).json(sortedProducts);
+})
+
+app.listen(5000, () => {
+    console.log('listening on port 5000...')
+})
